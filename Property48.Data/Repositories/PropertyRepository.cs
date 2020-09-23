02ea61d55048e0df7ff6f67f7ea25df223ab9df0
Keanan Chetty <Keanank9Chetty08@gmail.com>
2020-09-23 16:44:36 +0200

    Just Add a Link

--- Property48.Data/Repositories/PropertyRepository.cs
+++ Property48.Data/Repositories/PropertyRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Property48.Core.Entities;
 using Property48.Core.Repositories;
+using Property48.Data.DbConnection;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,15 +10,23 @@ namespace Property48.Data.Repositories
 {
     public class PropertyRepository : Repository<Property>, IPropertyRepository
     {
-      
+        public PropertyRepository(Property48DB context)
+            :base(context)
+        {
+                
+        }
+
         public Task<Property> GetPropertyByIdAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Property>> GetPropertyWithCitiesAsync()
+        public async Task<IEnumerable<Property>> GetPropertyWithCitiesAsync()
         {
             throw new NotImplementedException();
         }
+
+        //https://github.com/alopes2/Medium-MyMusic-Identity/blob/master/MyMusic.Data/Repositories/ArtistRepository.cs
+        //https://medium.com/swlh/building-a-nice-multi-layer-net-core-3-api-c68a9ef16368
     }
 }
